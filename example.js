6efe30b594bce9b8dff200277b4a1a748a5f2da3
niu <iamblessed@126.com>
2023-10-07 16:03:56 +0800

    delete: 删除旧代码

--- example.js
+++
@@ -1,9 +0,0 @@
-const source = './ooto.sketch';
-const fse = require('fs-extra');
-
-var parser = require('./index');
-parser(source, function(){
-    fse.copy('./output/html', './docs', err => {
-    });
-});
-
